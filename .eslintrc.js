2f2ae313bca347cb1809706aba448512eaa57da0
Miguel Ripoll <miguel.ripoll@outlook.com>
2022-07-16 19:21:10 +0200

    Add ESLint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {},
+  globalVariables: {
+    process: "readonly",
+    _dirname: "readonly",
+  },
+};
